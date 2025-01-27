2250a774cedcaabd2eea1022b49e1c9c8b204546
malexj <alexmaximovj@gmail.com>
2025-01-27 14:07:35 +0200

    Add cors

--- src/main/java/com/malex/test_app_backend/configuration/JwtFilterConfiguration.java
+++ src/main/java/com/malex/test_app_backend/configuration/JwtFilterConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class FilterConfiguration {
+public class JwtFilterConfiguration {
 
   @Bean
   public FilterRegistrationBean<JwtTokenFilter> jwtFilter(JwtTokenFilter jwtTokenFilter) {
--- src/main/java/com/malex/test_app_backend/controller/user/dto/UserRequest.java
+++ src/main/java/com/malex/test_app_backend/controller/user/dto/UserRequest.java
@@ -6,5 +6,7 @@ import java.util.List;
 public record UserRequest(
     @Schema(name = "userId", type = "number", description = "telegram user id", example = "12345")
         Long userId,
+//    String name,
+//    String nickname,
     @Schema(name = "references", description = "user references")
         List<UserRefRequest> references) {}
